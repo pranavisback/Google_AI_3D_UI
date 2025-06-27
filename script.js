032565fe8b9fc3c0b902e4cf5191f32346dbc116
Pranav Dhondiram Pawar <132260652+pranavisback@users.noreply.github.com>
2025-06-27 14:37:35 +0530

    Initial commit: Google AI 3D UI

---
+++ script.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x3f84f4,
+  backgroundColor: 0x0,
+  points: 10.00,
+  maxDistance: 25.00,
+  spacing: 15.00
+})
